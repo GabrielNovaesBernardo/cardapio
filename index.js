4982034064271301a24aa39d51571a0cb3488689
Gabriel Novaes <gabrielnovaisoficial@gmail.com>
2023-11-01 22:12:10 -0300

    .

---
+++ index.js
@@ -0,0 +1,200 @@
+//
+
+
+//hamburgueres
+// chicken
+//mostra/oculta adicionais
+function adict_chicken() {
+    var display = document.getElementById('chicken').style.display;
+    if(display == "none")
+        document.getElementById('chicken').style.display = 'block';
+    else
+        document.getElementById('chicken').style.display = 'none';
+}
+
+// la-casa
+//mostra/oculta adicionais
+function adict_laCasa() {
+    var display = document.getElementById('la_casa').style.display;
+    if(display == "none")
+        document.getElementById('la_casa').style.display = 'block';
+    else
+        document.getElementById('la_casa').style.display = 'none';
+}
+
+//nachos
+//mostra/oculta adicionais
+function adict_nachos() {
+    var display = document.getElementById('nachos').style.display;
+    if(display == "none")
+        document.getElementById('nachos').style.display = 'block';
+    else
+        document.getElementById('nachos').style.display = 'none';
+}
+
+//smash
+//mostra/oculta adicionais
+function adict_smash() {
+    var display = document.getElementById('smash').style.display;
+    if(display == "none")
+        document.getElementById('smash').style.display = 'block';
+    else
+        document.getElementById('smash').style.display = 'none';
+}
+
+//smash II
+//mostra/oculta adicionais
+function adict_smashII() {
+    var display = document.getElementById('smashII').style.display;
+    if(display == "none")
+        document.getElementById('smashII').style.display = 'block';
+    else
+        document.getElementById('smashII').style.display = 'none';
+}
+
+//picanha
+//mostra/oculta adicionais
+function adict_picanha() {
+    var display = document.getElementById('picanha').style.display;
+    if(display == "none")
+        document.getElementById('picanha').style.display = 'block';
+    else
+        document.getElementById('picanha').style.display = 'none';
+}
+
+//picanhaII
+//mostra/oculta adicionais
+function adict_picanhaII() {
+    var display = document.getElementById('picanhaII').style.display;
+    if(display == "none")
+        document.getElementById('picanhaII').style.display = 'block';
+    else
+        document.getElementById('picanhaII').style.display = 'none';
+}
+
+//kids
+//mostra/oculta adicionais
+function adict_kids() {
+    var display = document.getElementById('kids').style.display;
+    if(display == "none")
+        document.getElementById('kids').style.display = 'block';
+    else
+        document.getElementById('kids').style.display = 'none';
+}
+
+//onion
+//mostra/oculta adicionais
+function adict_onion() {
+    var display = document.getElementById('onion').style.display;
+    if(display == "none")
+        document.getElementById('onion').style.display = 'block';
+    else
+        document.getElementById('onion').style.display = 'none';
+}
+
+//bacon
+//mostra/oculta adicionais
+function adict_bacon() {
+    var display = document.getElementById('bacon').style.display;
+    if(display == "none")
+        document.getElementById('bacon').style.display = 'block';
+    else
+        document.getElementById('bacon').style.display = 'none';
+}
+
+//queijo cremoso
+//mostra/oculta adicionais
+function adict_queijoCremoso() {
+    var display = document.getElementById('queijoCremoso').style.display;
+    if(display == "none")
+        document.getElementById('queijoCremoso').style.display = 'block';
+    else
+        document.getElementById('queijoCremoso').style.display = 'none';
+}
+
+//route calabresa
+//mostra/oculta adicionais
+function adict_routeCalabresa() {
+    var display = document.getElementById('routeCalabresa').style.display;
+    if(display == "none")
+        document.getElementById('routeCalabresa').style.display = 'block';
+    else
+        document.getElementById('routeCalabresa').style.display = 'none';
+}
+
+//paulistinha
+//verifica o dia da semana pra mostrar ele no site
+let dataAtual = new Date();
+
+//(0 é domingo, 1 é segunda-feira, ..., 4 é quinta-feira)
+let diaDaSemana = dataAtual.getDay();
+
+// Verifique se hoje é quinta-feira
+if(diaDaSemana === 5) {
+    // Se for quinta-feira
+    document.getElementById("paulistinha").style.display = "block";
+} else {
+    // Se não for quinta-feira
+    document.getElementById("paulistinha").style.display = "none";
+}
+
+//mostra/oculta adicionais
+function adict_paulistinha() {
+    var display = document.getElementById('paulistinha').style.display;
+    if(display == "none")
+        document.getElementById('paulistinha').style.display = 'block';
+    else
+        document.getElementById('paulistinha').style.display = 'none';
+}
+
+//burguer
+//mostra/oculta adicionais
+function adict_burguer() {
+    var display = document.getElementById('burguer').style.display;
+    if(display == "none")
+        document.getElementById('burguer').style.display = 'block';
+    else
+        document.getElementById('burguer').style.display = 'none';
+}
+
+//BBQ
+//mostra/oculta adicionais
+function adict_bbq() {
+    var display = document.getElementById('bbq').style.display;
+    if(display == "none")
+        document.getElementById('bbq').style.display = 'block';
+    else
+        document.getElementById('bbq').style.display = 'none';
+}
+
+//BBQ II
+//mostra/oculta adicionais
+function adict_bbqII() {
+    var display = document.getElementById('bbqII').style.display;
+    if(display == "none")
+        document.getElementById('bbqII').style.display = 'block';
+    else
+        document.getElementById('bbqII').style.display = 'none';
+}
+
+//salada
+//mostra/oculta adicionais
+function adict_salada() {
+    var display = document.getElementById('salada').style.display;
+    if(display == "none")
+        document.getElementById('salada').style.display = 'block';
+    else
+        document.getElementById('salada').style.display = 'none';
+}
+
+//australian
+//mostra/oculta adicionais
+function adict_australian() {
+    var display = document.getElementById('australian').style.display;
+    if(display == "none")
+        document.getElementById('australian').style.display = 'block';
+    else
+        document.getElementById('australian').style.display = 'none';
+}
+
+
